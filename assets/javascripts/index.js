5da65a9e524b30c94d245ad940af63b5bfcadea0
jakobsmith-iterative <jakob@iterative.consulting>
2020-11-14 10:30:13 -0700

    queueing jquery correctly with webpack

--- assets/javascripts/index.js
+++ assets/javascripts/index.js
@@ -1,4 +1,6 @@
-import 'jquery'
 import 'jquery-ui'
+import 'jquery-ui/ui/effect.js'
 import 'multiscroll.js'
 import 'multiscroll.js/jquery.multiscroll.css'
+
+import './multiscroll-init.js'
--- webpack.config.js
+++ webpack.config.js
@@ -1,45 +1,53 @@
 const path = require('path');
 const miniCssPlugin = require('mini-css-extract-plugin');
 const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
+const webpack = require("webpack");
 
 module.exports = {
-    entry: {
-        site: ['./assets/javascripts/index.js', './assets/stylesheets/index.scss']
-    },
-    output: {
-        filename: 'assets/javascripts/[name].js',
-        path: path.resolve(__dirname, '.tmp/dist')
-    },
-    module: {
-        rules: [
-            {
-                test: /\.scss$/,
-                use: [miniCssPlugin.loader, 'css-loader', 'sass-loader']
-            },
-            {
-                test: /\.css$/,
-                use: [miniCssPlugin.loader, 'css-loader']
-            }
-        ]
-    },
-    optimization: {
-        minimize: true,
-        minimizer: [
-          new CssMinimizerPlugin({
-            minimizerOptions: {
-              preset: [
-                'default',
-                {
-                  discardComments: { removeAll: true },
-                },
-              ],
-            },
-          }),
-        ],
-      },
-    plugins: [
-        new miniCssPlugin({
-            filename: 'assets/stylesheets/[name].css'
-        })
+  entry: {
+    site: [
+      './assets/javascripts/index.js',
+      './assets/stylesheets/index.scss'
     ]
+  },
+  output: {
+    filename: 'assets/javascripts/[name].js',
+    path: path.resolve(__dirname, '.tmp/dist')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        use: [miniCssPlugin.loader, 'css-loader', 'sass-loader']
+      },
+      {
+        test: /\.css$/,
+        use: [miniCssPlugin.loader, 'css-loader']
+      }
+    ]
+  },
+  optimization: {
+    minimize: true,
+    minimizer: [
+      new CssMinimizerPlugin({
+        minimizerOptions: {
+          preset: [
+            'default',
+            {
+              discardComments: { removeAll: true },
+            },
+          ],
+        },
+      }),
+    ],
+  },
+  plugins: [
+    new miniCssPlugin({
+      filename: 'assets/stylesheets/[name].css'
+    }),
+    new webpack.ProvidePlugin({
+      $: 'jquery',
+      jQuery: 'jquery'
+    })
+  ]
 }
